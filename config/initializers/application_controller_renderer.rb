6aa7e8312f9edf81f3ea98fa1bc60ea7e1ee2624
Kamezaki-jp <champagne.es335@gmail.com>
2021-02-03 03:23:08 +0000

    [Finish] 4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '95149e1ce57c54500c2a80802c732d5d3bb926dbb1bc1122abb5986ada45575492adc5c6034e3e3e4619f6c736ec1c0551ac2a35dabec6781eb2e59a8fd81821'
